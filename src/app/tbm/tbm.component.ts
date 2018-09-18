aaa70e489671cd4fd576de02ec743f20a8fcfe02
Johannes Maurer <johannesmaurer@Johanness-MacBook-Pro.local>
2018-09-18 11:15:19 +0200

    timeline index function

--- src/app/tbm/tbm.component.ts
+++ src/app/tbm/tbm.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Lawelement } from '../lawelement';
 
 @Component({
   selector: 'app-tbm',
@@ -17,5 +18,26 @@ export class TbmComponent implements OnInit {
   ngOnInit() {
   }
 
+  sortData(dataService: DataService) {
+   let tempArray = [];
+   for(var i=0; i<dataService.lawelementArray.length; i++) {
+     tempArray.push(dataService.lawelementArray[i]);
+   }
+    let outputArray = tempArray.sort((first, second): number => {
+     if (first.claimant.time < second.claimant.time) return -1;
+     if (first.claimant.time > second.claimant.time) return 1;
+     return 0;
+   })
+   return outputArray;
+  }
+
+  getIndexOfLawelement(element: any) {
+    for(var i=0; i<this.dataService.lawelementArray.length; i++) {
+      if(element === this.dataService.lawelementArray[i]) {
+        return i;
+      }
+    }
+    return null;
+  }
 
 }
